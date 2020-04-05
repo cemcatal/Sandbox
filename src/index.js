e481a438c075a36548a765d3917d1aa063982971
Cem Catal <alicemcatal@gmail.com>
2020-04-05 14:14:30 +0300

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,19 @@
+//Create a react app from scratch.
+//It should display a h1 heading.
+//It should display an unordered list (bullet points).
+//It should contain 3 list elements.
+
+import React from "react";
+import ReactDOM from "react-dom";
+
+ReactDOM.render(
+  <div>
+    <h1>My Favorite Foods</h1>
+    <ul>
+      <li> Beyti </li>
+      <li> Et Haslama </li>
+      <li> Ali Nazik </li>
+    </ul>
+  </div>,
+  document.getElementById("root")
+);
